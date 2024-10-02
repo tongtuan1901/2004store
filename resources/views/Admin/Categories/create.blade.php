05bd59a12b6798bacb06edb7fc83a70e0e67cecf
tuyenph123 <tuyenvc33@gmail.com>
2024-10-02 22:44:26 +0700

    admin categories

---
+++ resources/views/Admin/Categories/create.blade.php
@@ -0,0 +1,32 @@
+@extends('Admin/layouts/master/master')
+
+@section('content')
+<div class="w-full relative mb-4">
+    <form action="{{ route('admin-categories.store') }}" method="POST"> <!-- Thay đổi action ở đây -->
+        @csrf <!-- Thêm token CSRF để bảo mật -->
+        <div class="flex-auto p-0 md:p-4">
+            <div class="mb-2">
+                <label for="title" class="font-medium text-sm text-slate-600 dark:text-slate-400">Tên danh mục :</label>
+                <input type="text" id="title" name="name"
+                 class="form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-brand-500 dark:focus:border-brand-500  dark:hover:border-slate-700"
+                 placeholder="Tên sản phẩm" @error('name') is-invalid @enderror
+                 value="{{ old('name') }}"
+                 required>
+                 @error('name')
+                     <div class="invalid-feedback">
+                        {{ $message }}
+                     </div>
+                 @enderror
+            </div>
+
+            <div class="">
+                <button type="submit" class="px-2 py-2 lg:px-4 bg-brand text-white text-sm rounded hover:bg-brand-600 border border-brand-500">Thêm sản phẩm</button>
+                <button type="button" class="px-2 py-2 lg:px-4 bg-transparent text-brand text-sm rounded transition hover:bg-brand-500 hover:text-white border border-brand font-medium">
+                    <a href="{{ route('admin-categories.index') }}">Quay lại</a>
+                </button>
+            </div>
+        </div><!--end card-body-->
+    </form>
+</div><!--end card-->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoriesController; // Sửa ở đây
 use App\Http\Controllers\AdminProductsController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,10 +9,16 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is~ where you can register web routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::resource('admin-products', AdminProductsController::class);
+
+// Danh mục
+Route::resource('admin-categories', AdminCategoriesController::class);
+Route::get('add-category', [AdminCategoriesController::class, 'create']);
+Route::post('store-category', [AdminCategoriesController::class, 'store']);
+
