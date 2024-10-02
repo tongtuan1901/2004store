86e9fa78b3b75f343ef47781ef403ed8fd40ac4a
dylantran04 <dylan.tran@mps-asia.com>
2024-10-02 16:22:31 +0700

    phước đẩy cắt giao diện, cn list products

---
+++ resources/views/Admin/layouts/master/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" class="scroll-smooth group" data-sidebar="brand" dir="ltr">
+    
+<!-- Mirrored from mannatthemes.com/robotech/default/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Nov 2023 15:34:21 GMT -->
+<head>
+        <meta charset="utf-8" />
+        <title>Robotech - Admin & Dashboard Template</title>
+        <meta  name="viewport"  content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta  content="Tailwind Multipurpose Admin & Dashboard Template"  name="description"/>
+        <meta content="" name="Mannatthemes" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/css/tailwind.min.css') }}" />
+        
+        <!-- Css -->
+        <!-- Main Css -->
+        <link rel="stylesheet" href="{{ asset('assets/libs/icofont/icofont.min.css') }}">
+        <link href="{{ asset('assets/libs/flatpickr/flatpickr.min.css') }}" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('assets/css/tailwind.min.css') }}">
+
+    </head>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,12 +8,10 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is~ where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('admin-products', AdminProductsController::class);
