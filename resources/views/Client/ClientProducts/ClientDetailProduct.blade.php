a7c1b9e2795fd703d8ffed8750141818413f0dd6
lu0n9 <luong51024@gmail.com>
2024-11-18 12:42:32 +0700

    l

--- resources/views/Client/ClientProducts/ClientDetailProduct.blade.php
+++ resources/views/Client/ClientProducts/ClientDetailProduct.blade.php
@@ -338,41 +338,8 @@
                 </div>
             </div>
 
-        </div>
+        <div>
             <div class="comments-section mt-4">
-                {{-- <h5>Bình luận:</h5>
-                @if($productDetail->comments)
-    @foreach ($productDetail->comments as $comment)
-        <div class="comment-item mb-3">
-            <div class="comment-header">
-                <strong>{{ $comment->user->name }}</strong>
-                <span class="text-muted">{{ $comment->created_at->diffForHumans() }}</span>
-            </div>
-            <div class="comment-content">
-                {{ $comment->content }}
-            </div>
-        </div>
-    @endforeach
-@endif
-            </div>
-            @auth
-                <form method="POST" action="{{ route('client-products.comments.store', $productDetail->id) }}">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="content" class="form-label">Viết bình luận của bạn:</label>
-                        <textarea name="content" id="content" class="form-control" rows="3" required></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Gửi bình luận</button>
-                </form>
-            @else
-                <div class="mb-3">
-                    <label for="content" class="form-label">Viết bình luận của bạn:</label>
-                    <textarea name="content" id="content" class="form-control" rows="3" required></textarea>
-                </div>
-                <p class="text-muted">Bạn phải <a class="btn btn-success" href="{{ route('client-login.index') }}">đăng nhập</a>
-                    để bình luận.</p>
-            @endauth --}}
-
             <div class="btn-group mb-3">
                 <button id="btn-comments" class="btn btn-primary">Bình luận</button>
                 <button id="btn-reviews" class="btn btn-secondary">Đánh giá</button>
@@ -455,7 +422,7 @@
                 });
             </script>
             
-
+        </div>
         <div class="main-product-relate">
             <div class="section-title-all">
                 <h2>Sản phẩm liên quan</h2>
