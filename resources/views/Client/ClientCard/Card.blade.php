fa7f9c98555d6320564f98f4311a0723586403aa
tuyenph123 <tuyenvc33@gmail.com>
2024-11-19 16:16:22 +0700

    sưa giao diện

--- resources/views/Client/ClientCard/Card.blade.php
+++ resources/views/Client/ClientCard/Card.blade.php
@@ -1,5 +1,6 @@
 @extends('Client.layouts.paginate.master')
 @section('contentClient')
+
     <main class="main-layout">
         <div class="main-cart">
             <div class="main-cart-breadcrumb" title="Giỏ hàng">
@@ -11,15 +12,14 @@
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="/" aria-label="Trang chủ" title="Trang chủ">Trang
                                     chủ</a></li>
-                                    @if(Session::has('success'))
-        <div class="alert alert-success">{{ Session::get('success') }}</div>
-    @endif
+                            @if (Session::has('success'))
+                                <div class="alert alert-success">{{ Session::get('success') }}</div>
+                            @endif
 
-    @if(empty($cart))
-        <p>Giỏ hàng của bạn đang trống.</p>
-    @else
-   
-                            <li class="breadcrumb-item active"><span>Giỏ hàng</span></li>
+                            @if (empty($cart))
+                                <p>Giỏ hàng của bạn đang trống.</p>
+                            @else
+                                <li class="breadcrumb-item active"><span>Giỏ hàng</span></li>
                         </ol>
                     </div>
                 </div>
@@ -34,86 +34,88 @@
                         <div class="main-cart-data-full">
                             <div class="main-cart-data-full-list">
                                 <div class="main-cart-data-full-item" data-id="120912660">
-                                   
-                                   
+
+
                                 </div>
 
-                               
+
                                 @foreach ($cart as $item)
-                                <div class="main-cart-data-full-item" data-id="120912709">
-                                    <div class="main-cart-data-full-item-image">
-                                        <a href="/two-line-halter-neck-top">
-                                            <img title="TWO LINE HALTER NECK TOP"
-                                            @if($item->variation && $item->variation->image)
-    <img src="{{ Storage::url($item->variation->image->image_path) }}" 
+                                    <div class="main-cart-data-full-item" data-id="120912709">
+                                        <div class="main-cart-data-full-item-image">
+                                            <a href="/two-line-halter-neck-top">
+                                                <img title="TWO LINE HALTER NECK TOP"
+                                                    @if ($item->variation && $item->variation->image) <img src="{{ Storage::url($item->variation->image->image_path) }}"
          alt="{{ $item->product->name }}"
          title="{{ $item->product->name }}"/>
 @else
-    <img src="{{ asset('path/to/default/image.jpg') }}" 
-         alt="Default Image"/>
-@endif
-                                        </a>
-                                    </div>
-                                    <div class="main-cart-data-full-item-info">
-                                        <h3 class="main-cart-data-full-item-info-title"><a
-                                                href="Lỗi liquid: Exception has been thrown by the target of an invocation."
-                                                title="TWO LINE HALTER NECK TOP">{{$item->product->name }}</a></h3>
-                                        <div class="main-cart-data-full-item-info-price">
-                                            <label>Giá: </label>
-
-                                            @if ($item->variation)
-        {{ number_format($item->variation->price, 0, ',', '.') }}₫
-    @else
-        {{ number_format($item->product->price, 0, ',', '.') }}₫
-    @endif
+    <img src="{{ asset('path/to/default/image.jpg') }}"
+         alt="Default Image"/> @endif
+                                                    </a>
                                         </div>
+                                        <div class="main-cart-data-full-item-info">
+                                            <h3 class="main-cart-data-full-item-info-title"><a
+                                                    href="Lỗi liquid: Exception has been thrown by the target of an invocation."
+                                                    title="TWO LINE HALTER NECK TOP">{{ $item->product->name }}</a></h3>
+                                            <div class="main-cart-data-full-item-info-price">
+                                                <label>Giá: </label>
 
-                                        <div class="main-cart-data-full-item-info-variant">
-    <label>Phiên bản: </label>
-    @if ($item->variation)
-                    <span>{{ $item->variation->color->color }} / {{ $item->variation->size->size }}</span>
-                @else
-                    <p>Không có biến thể được chọn. {{ dd($item->variation) }}</p>
-                @endif
-</div>
+                                                @if ($item->variation)
+                                                    {{ number_format($item->variation->price, 0, ',', '.') }}₫
+                                                @else
+                                                    {{ number_format($item->product->price, 0, ',', '.') }}₫
+                                                @endif
+                                            </div>
 
-                                        <div class="main-cart-data-full-item-info-quantity shop-quantity-wrap">
-                                            <label>Số lượng</label>
-                                            <div class="shop-quantity">
-                                                <button type="button" data-type="shop-quantity-minus"
-                                                    title="Giảm">-</button>
-                                                <input type="number" name="quantity_120912709" value="{{ $item->quantity }}"
-                                                    min="1" readonly data-vid="120912709">
-                                                <button type="button" data-type="shop-quantity-plus"
-                                                    title="Tăng">+</button>
+                                            <div class="main-cart-data-full-item-info-variant">
+                                                <label>Phiên bản: </label>
+                                                @if ($item->variation)
+                                                    <span>{{ $item->variation->color->color }} /
+                                                        {{ $item->variation->size->size }}</span>
+                                                @else
+                                                    <p>Không có biến thể được chọn. {{ dd($item->variation) }}</p>
+                                                @endif
+                                            </div>
+
+                                            <div class="main-cart-data-full-item-info-quantity shop-quantity-wrap">
+                                                <label>Số lượng</label>
+                                                <div class="shop-quantity">
+                                                    <button type="button" data-type="shop-quantity-minus"
+                                                        title="Giảm">-</button>
+                                                    <input type="number" name="quantity_120912709"
+                                                        value="{{ $item->quantity }}" min="1" readonly
+                                                        data-vid="120912709">
+                                                    <button type="button" data-type="shop-quantity-plus"
+                                                        title="Tăng">+</button>
+                                                </div>
                                             </div>
                                         </div>
-
-
-                                        <div class="main-cart-data-full-item-info-total hidden d-none" hidden>
-
-                                            <label>Thành tiền: </label>
-                                            <span>1.390.000₫</span>
-
-                                        </div>
-                                        <div class="main-cart-data-full-item-info-remove">
-                                            <a href="/cart/change?line=2&quantity=0" title="Xóa sản phẩm">Xoá sản phẩm</a>
+                                        <div class="main-cart-data-full-item-action">
+                                            <form action="{{ route('cart.remove', $item->id) }}" method="POST"
+                                                style="display: inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" title="Xóa sản phẩm">
+                                                    <svg class="sherah-color2__fill" xmlns="http://www.w3.org/2000/svg"
+                                                        width="16.247" height="18.252" viewBox="0 0 16.247 18.252">
+                                                        <g id="Icon" transform="translate(-160.007 -18.718)">
+                                                            <path id="Path_484" data-name="Path 484"
+                                                                d="M185.344,88.136c0,1.393,0,2.786,0,4.179-.006,1.909-1.523,3.244-3.694,3.248q-3.623.007-7.246,0c-2.15,0-3.682-1.338-3.687-3.216q-.01-4.349,0-8.7a.828.828,0,0,1,.822-.926.871.871,0,0,1,1,.737c.016.162.006.326.006.489q0,4.161,0,8.321c0,1.061.711,1.689,1.912,1.69q3.58,0,7.161,0c1.2,0,1.906-.631,1.906-1.695q0-4.311,0-8.622a.841.841,0,0,1,.708-.907.871.871,0,0,1,1.113.844C185.349,85.1,185.343,86.618,185.344,88.136Z"
+                                                                transform="translate(-9.898 -58.597)" />
+                                                            <path id="Path_485" data-name="Path 485"
+                                                                d="M164.512,21.131c0-.517,0-.98,0-1.443.006-.675.327-.966,1.08-.967q2.537,0,5.074,0c.755,0,1.074.291,1.082.966.005.439.005.878.009,1.317a.615.615,0,0,0,.047.126h.428c1,0,2,0,3,0,.621,0,1.013.313,1.019.788s-.4.812-1.04.813q-7.083,0-14.165,0c-.635,0-1.046-.327-1.041-.811s.4-.786,1.018-.789C162.165,21.127,163.3,21.131,164.512,21.131Zm1.839-.021H169.9v-.764h-3.551Z"
+                                                                transform="translate(0 0)" />
+                                                            <path id="Path_486" data-name="Path 486"
+                                                                d="M225.582,107.622c0,.9,0,1.806,0,2.709a.806.806,0,0,1-.787.908.818.818,0,0,1-.814-.924q0-2.69,0-5.38a.82.82,0,0,1,.81-.927.805.805,0,0,1,.79.9C225.585,105.816,225.582,106.719,225.582,107.622Z"
+                                                                transform="translate(-58.483 -78.508)" />
+                                                            <path id="Path_487" data-name="Path 487"
+                                                                d="M266.724,107.63c0-.9,0-1.806,0-2.709a.806.806,0,0,1,.782-.912.818.818,0,0,1,.818.919q0,2.69,0,5.38a.822.822,0,0,1-.806.931c-.488,0-.792-.356-.794-.938C266.721,109.411,266.724,108.521,266.724,107.63Z"
+                                                                transform="translate(-97.561 -78.509)" />
+                                                        </g>
+                                                    </svg>
+                                                </button>
+                                            </form>
                                         </div>
                                     </div>
-                                    <div class="main-cart-data-full-item-action">
-    <form action="{{ route('cart.remove', $item->id) }}" method="POST" style="display: inline;">
-        @csrf
-        @method('DELETE')
-        <button type="submit" title="Xóa sản phẩm">
-            <svg version="1.1" x="0px" y="0px" viewBox="0 0 325.284 325.284" style="enable-background:new 0 0 325.284 325.284;" xml:space="preserve">
-                <g>
-                    <!-- SVG content here -->
-                </g>
-            </svg>
-        </button>
-    </form>
-</div>
-                                </div>
                                 @endforeach
                                 @endif
                                 <div class="main-cart-data-full-note">
@@ -126,16 +128,16 @@
                                 <div class="main-cart-data-full-total-sub">
                                     <div class="main-cart-data-full-total-sub-price">
 
-                                    @php
-    $totalPrice = 0;
-    foreach ($cart as $item) {
-        if ($item->variation) {
-            $totalPrice += $item->variation->price * $item->quantity;
-        } else {
-            $totalPrice += $item->product->price * $item->quantity;
-        }
-    }
-@endphp
+                                        @php
+                                            $totalPrice = 0;
+                                            foreach ($cart as $item) {
+                                                if ($item->variation) {
+                                                    $totalPrice += $item->variation->price * $item->quantity;
+                                                } else {
+                                                    $totalPrice += $item->product->price * $item->quantity;
+                                                }
+                                            }
+                                        @endphp
 
                                         <label>Tổng tiền:</label>
                                         <span>{{ number_format($totalPrice, 0, ',', '.') }}₫</span>
@@ -147,15 +149,10 @@
                                         Bạn cũng có thể nhập mã giảm giá ở trang thanh toán.</div>
                                 </div>
                                 <div class="main-cart-data-full-invoice">
-                                    <div class="main-cart-data-full-invoice-head">
-                                        <input type="checkbox" id="main-cart-data-full-invoice"
-                                            name="main-cart-data-full-invoice" autocomplete='off'>
-                                        <label for="main-cart-data-full-invoice">Xuất hóa đơn</label>
-                                    </div>
+
                                     <div class="main-cart-data-full-invoice-data">
-                                        <input type="text" name="Công ty"
-                                            id="main-cart-data-full-invoice-data-company" value=""
-                                            placeholder="Tên công ty">
+                                        <input type="text" name="Công ty" id="main-cart-data-full-invoice-data-company"
+                                            value="" placeholder="Tên công ty">
                                         <input type="text" name="Mã số thuế" id="main-cart-data-full-invoice-data-tax"
                                             value="" placeholder="Mã số thuế">
                                         <input type="text" name="Người đại diện"
@@ -166,10 +163,13 @@
                                     </div>
                                 </div>
                                 <div class="main-cart-data-full-total-action">
-                                    <a href="{{route('client-home.index')}}" title="Tiếp tục mua hàng">Tiếp tục mua hàng</a>
+                                    <a href="{{ route('client-home.index') }}" title="Tiếp tục mua hàng">Tiếp tục mua
+                                        hàng</a>
 
-                                    <a href="{{route('client-checkout.index')}}" title="Thanh toán">Thanh toán</a>
-                                  
+                                </div>
+                                <div class="text-center" style="padding-bottom: 40px">
+                                    <a class="ft2" href="{{ route('client-checkout.index') }}"
+                                        title="Thanh toán">Thanh toán</a>
 
                                 </div>
                             </div>
@@ -180,5 +180,6 @@
         </div>
         <div class="sapo-buyxgety-module-cart-v2 container"></div>
         <div class="sapo-buyxgety-module-cart-v2 container"></div>
+
     </main>
 @endsection
